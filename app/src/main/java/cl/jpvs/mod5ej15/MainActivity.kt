8b36b9d46267f4f5bff4e3a5e93fe9fd0dc7ae99
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-18 18:52:03 -0400

    Initial commit

---
+++ app/src/main/java/cl/jpvs/mod5ej15/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.jpvs.mod5ej15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
